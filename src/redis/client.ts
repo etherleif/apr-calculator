59f25cd10dc39bc363bf7d36688ebc4b9013cc61
Leif <leifleiser@MBP-von-Leif.fritz.box>
2023-02-01 14:08:28 +0100

    getting redis client up and running

---
+++ src/redis/client.ts
@@ -0,0 +1,12 @@
+import config from "../config";
+import { createClient } from "redis";
+
+export const client = createClient({
+  url: config.redisUrl, 
+});
+client.on('error', err => console.log('Redis Client Error', err));
+
+export const startRedis = async () => {
+    await client.connect();
+}
+
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,33 @@
-import express, { Application } from 'express';
-import compression from 'compression';
-import cors from 'cors';
-import { queryParser } from 'express-query-parser';
-import bodyParser from 'body-parser';
-import config from './config';
+import express, { Application } from "express";
+import compression from "compression";
+import cors from "cors";
+import { queryParser } from "express-query-parser";
+import bodyParser from "body-parser";
+import config from "./config";
+import {  startRedis } from "./redis/client";
 
-const app: Application = express();
-app.use(compression());
-app.use(cors());
-app.use(bodyParser.json());
+const startExpress = () => {
+  const app: Application = express();
 
-app.use(
+  app.use(compression());
+  app.use(cors());
+  app.use(bodyParser.json());
+
+  app.use(
     queryParser({
       parseNull: true,
       parseUndefined: true,
       parseBoolean: true,
-      parseNumber: true
+      parseNumber: true,
     })
-  )
+  );
 
-  app.listen(config.port, () => console.log(`Farms server is listening on port ${config.port}!`));
+  app.listen(config.port, () =>
+    console.log(`Farms server is listening on port ${config.port}!`)
+  );
+};
+
+(async () => {
+  await startRedis();
+  startExpress();
+})();
